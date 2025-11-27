e69eff0c3d0757596762c96dfd5c826f94759584
Varivonchik Artem <varivonchik.core@gmail.com>
2025-11-27 23:36:39 +0300

    feat(pages): implement not found page and invalid route handling

--- src/app/providers/router/routes/routeConfiguration/routeConfiguration.tsx
+++ src/app/providers/router/routes/routeConfiguration/routeConfiguration.tsx
@@ -1,17 +1,24 @@
 import { RouteProps } from "react-router-dom";
 import { MainPage } from "@/pages/MainPage";
+import { NotFoundPage } from "@/pages/NotFoundPage";
 
-export enum AppRoutes {
-  MAIN_PAGE = "main",
+export enum AppRouteName {
+  MAIN = "main",
+  NOT_FOUND = "not_found",
 }
 
-export const RoutePath: Record<AppRoutes, string> = {
-  [AppRoutes.MAIN_PAGE]: "/",
+export const AppRoutePath: Record<AppRouteName, string> = {
+  [AppRouteName.MAIN]: "/",
+  [AppRouteName.NOT_FOUND]: "*",
 };
 
-export const routeConfiguration: Record<AppRoutes, RouteProps> = {
-  [AppRoutes.MAIN_PAGE]: {
-    path: RoutePath.main,
+export const routeConfiguration: Record<AppRouteName, RouteProps> = {
+  [AppRouteName.MAIN]: {
+    path: AppRoutePath.main,
     element: <MainPage />,
+  },
+  [AppRouteName.NOT_FOUND]: {
+    path: AppRoutePath.not_found,
+    element: <NotFoundPage />,
   },
 };
